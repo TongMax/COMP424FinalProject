a044a36a5f26b02470c9951443c6cbbc1d8368bb
TongMax <maxwell.tong@gmail.com>
2022-11-24 02:36:22 -0500

    almost done

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -6,56 +6,25 @@ import math
 import numpy as np
 from copy import deepcopy
 
-
-@register_agent("student_agent")
-class StudentAgent(Agent):
-    """
-    A dummy class for your implementation. Feel free to use this class to
-    add any helper functionalities needed for your agent.
-    """
-
-    def __init__(self):
-        super(StudentAgent, self).__init__()
-        self.name = "StudentAgent"
-        self.dir_map = {
-            "u": 0,
-            "r": 1,
-            "d": 2,
-            "l": 3,
-        }
-
-    def step(self, chess_board, my_pos, adv_pos, max_step):
-        """
-        Implement the step function of your agent here.
-        You can use the following variables to access the chess board:
-        - chess_board: a numpy array of shape (x_max, y_max, 4)
-        - my_pos: a tuple of (x, y)
-        - adv_pos: a tuple of (x, y)
-        - max_step: an integer
-
-        You should return a tuple of ((x, y), dir),
-        where (x, y) is the next position of your agent and dir is the direction of the wall
-        you want to put on.
-
-        Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
-        """
-        self.start_pos = my_pos
-        self.adv_pos = adv_pos
-        self.chess_board = chess_board
-        self.max_step = max_step
-        self.next_pos = my_pos
-
-
-        root = MonteCarloTreeSearchNode(deepcopy(self.chess_board))
-        selected_node = root.best_action()
-        print(selected_node)
-
-        # dummy return
-        return my_pos, self.dir_map["u"]
-
+class MonteCarloTreeSearchNode():
+    def __init__(self, cur_pos, adv_pos, dir, chess_board, parent=None, parent_action=None):
+            self.chess_board = chess_board
+            self.cur_pos = cur_pos
+            self.adv_pos = adv_pos
+            self.dir = dir
+            self.parent = parent
+            self.parent_action = parent_action
+            self.children = []
+            self._number_of_visits = 0
+            self._results = dict()
+            self._results[1] = 0
+            self._results[-1] = 0
+            self._untried_actions = None
+            self._untried_actions = self.untried_actions()
+            return
     def untried_actions(self):
 
-        self._untried_actions = self.state.get_legal_actions()
+        self._untried_actions = self.get_legal_actions()
         return self._untried_actions
 
     def q(self):
@@ -69,26 +38,28 @@ class StudentAgent(Agent):
     def expand(self):
         
         action = self._untried_actions.pop()
-        next_state = self.state.move(action)
-        child_node = MonteCarloTreeSearchNode(
-            next_state, parent=self, parent_action=action)
+        next_pos, dir = action
+        next_state = self.move(action)
+        child_node = MonteCarloTreeSearchNode(next_pos, self.adv_pos, dir, next_state, parent=self, parent_action=action)
 
         self.children.append(child_node)
         return child_node 
 
     def is_terminal_node(self):
-        return self.state.is_game_over(self.state)
+        return self.is_game_over()
 
+
+    # This default policy needs to be done iterably in one
     def rollout(self):
-        current_rollout_state = self.state
-        isOver, occupied = current_rollout_state.is_game_over(self.state)
+        current_rollout_state = self.chess_board
+        isOver = False
         while not isOver:
-            
+            isOver, occupied = current_rollout_state.is_game_over()
             possible_moves = current_rollout_state.get_legal_actions()
             
             action = self.rollout_policy(possible_moves)
             current_rollout_state = current_rollout_state.move(action)
-        return current_rollout_state.game_result(occupied)
+        return self.game_result(occupied)
 
     def backpropagate(self, result):
         self._number_of_visits += 1.
@@ -138,8 +109,8 @@ class StudentAgent(Agent):
     def get_legal_actions(self):
             legal_actions_queue = set()
             # Use BFS
-            state_queue = [(self.start_pos, 0)]
-            visited = {tuple(self.start_pos)}
+            state_queue = [(self.cur_pos, 0)]
+            visited = {tuple(self.cur_pos)}
             is_reached = False
             while state_queue:
                 cur_pos, cur_step = state_queue.pop(0)
@@ -183,7 +154,7 @@ class StudentAgent(Agent):
             # print("These are the current neighbours: ",n)
             return n
 
-        def manhattan(cur_pos, end_pos):
+        def manhattan(cur_pos):
             cr, cc = cur_pos
             er, ec = self.adv_pos
             return (abs(cr - er) + abs(cc-ec))
@@ -192,7 +163,7 @@ class StudentAgent(Agent):
         pqueue = set()
         closedset = set()
         #Current point is the starting point
-        cur_pos = (self.start_pos, 0, manhattan(self.start_pos, self.adv_pos))
+        cur_pos = (self.cur_pos, 0, manhattan(self.cur_pos))
         # print("This is cur: ",cur_pos)
         #Add the starting point to the priorityQueue
         pqueue.add(cur_pos)
@@ -237,7 +208,7 @@ class StudentAgent(Agent):
                 else:
                     #If it isn't in the open set, calculate the G and H score for the node
                     next_g = cur_g + 1
-                    next_h = manhattan((nr, nc), self.adv_pos)
+                    next_h = manhattan((nr, nc))
                     #Set the parent to our current item
                     # next_pos.parent = cur_pos
                     #Add it to the set
@@ -246,8 +217,9 @@ class StudentAgent(Agent):
         return True, visitedCells
 
     def is_game_over(self):
-        
-        return self.aStar(self.state)
+        if self.parent:
+            return self.aStar(self.state)
+        return False, -1
 
     def game_result(self, occupied):
         board_size = len(self.chess_board)
@@ -262,20 +234,92 @@ class StudentAgent(Agent):
         (r,c), dir = action
         chessCopy = deepcopy(self.chess_board)
         chessCopy[r, c, self.dir_map[dir]] = True
+        (ar, ac), adir = self.simulate_adv(chessCopy)
+        chessCopy[ar, ac, adir] = True
+        self.adv_pos = ((ar, ac), adir)
+        self.cur_pos = (r,c)
+        self.dir = dir
         return chessCopy
 
-class MonteCarloTreeSearchNode():
-    def __init__(self, state, parent=None, parent_action=None):
-            self.state = state
-            self.parent = parent
-            self.parent_action = parent_action
-            self.children = []
-            self._number_of_visits = 0
-            self._results = dict(int)
-            self._results[1] = 0
-            self._results[-1] = 0
-            self._untried_actions = None
-            self._untried_actions = self.untried_actions()
-            return
+    def simulate_adv(self, chess_board):
+        ori_pos = deepcopy(self.adv_pos)
+        cur_board = deepcopy(chess_board)
+        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+        steps = np.random.randint(0, self.max_step + 1)
+
+        # Random Walk
+        for _ in range(steps):
+            r, c = self.adv_pos
+            dir = math.random.randint(0, 4)
+            m_r, m_c = moves[dir]
+            my_pos = (r + m_r, c + m_c)
+
+            # Special Case enclosed by Adversary
+            k = 0
+            while self.state[r, c, dir] or my_pos == self.adv_pos:
+                k += 1
+                if k > 300:
+                    break
+                dir = np.random.randint(0, 4)
+                m_r, m_c = moves[dir]
+                my_pos = (r + m_r, c + m_c)
+
+            if k > 300:
+                my_pos = ori_pos
+                break
+
+        # Put Barrier
+        dir = np.random.randint(0, 4)
+        r, c = my_pos
+        while cur_board[r, c, dir]:
+            dir = np.random.randint(0, 4)
+
+        return my_pos, dir
+
+@register_agent("student_agent")
+class StudentAgent(Agent):
+    """
+    A dummy class for your implementation. Feel free to use this class to
+    add any helper functionalities needed for your agent.
+    """
+
+    def __init__(self):
+        super(StudentAgent, self).__init__()
+        self.name = "StudentAgent"
+        self.dir_map = {
+            "u": 0,
+            "r": 1,
+            "d": 2,
+            "l": 3,
+        }
+
+    def step(self, chess_board, my_pos, adv_pos, max_step):
+        """
+        Implement the step function of your agent here.
+        You can use the following variables to access the chess board:
+        - chess_board: a numpy array of shape (x_max, y_max, 4)
+        - my_pos: a tuple of (x, y)
+        - adv_pos: a tuple of (x, y)
+        - max_step: an integer
+
+        You should return a tuple of ((x, y), dir),
+        where (x, y) is the next position of your agent and dir is the direction of the wall
+        you want to put on.
+
+        Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
+        """
+        self.start_pos = my_pos
+        self.adv_pos = adv_pos
+        self.chess_board = chess_board
+        self.max_step = max_step
+        self.next_pos = my_pos
+
+        root = MonteCarloTreeSearchNode(self.start_pos, self.adv_pos, -1, deepcopy(self.chess_board))
+        selected_node = root.best_action()
+        print(selected_node)
+        next_pos = selected_node.cur_pos
+        dir = selected_node.dir
+
+        return next_pos, dir
 
     
